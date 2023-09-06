980c12667089de4e8cdf11832311206724d47212
rohiitian <imrohit404@gmail.com>
2023-09-06 13:50:42 +0530

    First Commit
    A real-time chat room application using spring boot web socket step by step

---
+++ chatapproom/src/main/java/com/chat/app/chatapproom/ChatapproomApplication.java
@@ -0,0 +1,13 @@
+package com.chat.app.chatapproom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatapproomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatapproomApplication.class, args);
+	}
+
+}
